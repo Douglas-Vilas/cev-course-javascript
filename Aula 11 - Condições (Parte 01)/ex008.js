9276c9fbe89095e0717a23ee359da8cd0e6a11f3
Douglas Vilas <douglas_vilass@hotmail.com>
2022-11-30 00:57:59 -0300

    Update ex008.js

--- Aula 11 - Condições (Parte 01)/ex008.js
+++ Aula 11 - Condições (Parte 01)/ex008.js
@@ -1,2 +1,6 @@
-var spe = 60.0
+var spe = 66.0
 console.log(`The speed of your car is ${spe}km/h`)
+if (spe > 60) { //Simple condition
+    console.log(`You have exceeded the permitted speed. FINED!`)
+}
+console.log(`Always drive wearing a seat belt`)
