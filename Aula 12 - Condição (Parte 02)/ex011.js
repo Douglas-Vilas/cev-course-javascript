94d5022f6ef5a0382f7c0eeac4e0681451810fac
Douglas Vilas <douglas_vilass@hotmail.com>
2022-12-12 19:17:12 -0300

    Update ex011.js

--- Aula 12 - Condição (Parte 02)/ex011.js
+++ Aula 12 - Condição (Parte 02)/ex011.js
@@ -1,6 +1,9 @@
-var age = 12
-if (age < 18) {
-    console.log(`Under age`)
-} else {
-    console.log(`Legally of age`)
+var age = 67
+console.log(`You have ${age} years old`)
+if (age < 16) {
+    console.log(`Don't vote`)
+} else if (age < 18 || age > 65) {
+        console.log(`Optional vote`)
+}else {
+    console.log(`Compulsory vote`)
 }
