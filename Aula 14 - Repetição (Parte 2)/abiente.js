35e3bffe5832fb9c041a2c14586984fcf7b21f25
Douglas Vilas <douglas_vilass@hotmail.com>
2022-12-26 19:13:07 -0300

    Create abiente.js

---
+++ Aula 14 - Repetição (Parte 2)/abiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Step ${c}`) // Logic test do at the end.
+    c++
+} while (c <= 6)
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Step ${c}`) --> Logic test while at the beginning.
+    c++
+}
+*/
+
+/* 
+console.log(`It's all right?`)
+console.log(`It's all right?`)
+console.log(`It's all right?`)
+console.log(`It's all right?`)
+console.log(`It's all right?`)
+console.log(`It's all right?`)
+*/
