03d938f73c484e944c0311185736d619249230e9
Douglas Vilas <douglas_vilass@hotmail.com>
2023-01-04 21:00:08 -0300

    Update script.js

--- Aula 14 - Exercícios/ex017/script.js
+++ Aula 14 - Exercícios/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Hello!')
+function multiplicationTable() {
+    let num = document.querySelector('input#txtn')
+    let mul = document.querySelector('select#selmult')
+    if (num.value.length == 0) {          
+        alert('Please, enter a number!')    
+    } else {
+        let n = Number(num.value)           
+        let c = 1
+        mul.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.txt = `${n} x ${c} = ${n*c}`
+            item.value = `mul${c}`
+            mul.appendChild(item)
+            c++
+        }
+    }
+}//Problema na execução. Verificar. Não aparece a tabuada
