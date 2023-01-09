e51a11ccc2796f81501083ff7582c18c6beb6d9e
Douglas Vilas <douglas_vilass@hotmail.com>
2023-01-09 19:12:50 -0300

    Create objeto01.js

---
+++ Aula 17 - Próximos Passos/objeto01.js
@@ -0,0 +1,9 @@
+let friend = {name:'José', 
+gender: 'M', 
+weight: 85.4, fatten(p=0){
+    console.log('Fed up')
+    this.weight += p 
+}}
+
+friend.fatten(2)
+console.log(`${friend.name} weighs ${friend.weight}kg`)
